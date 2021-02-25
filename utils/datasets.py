d1931ee4d5b72e88b37fe51341047595cda642de
Clément Bonnet <clement.bonnet16@gmail.com>
2021-02-25 19:47:31 +0100

    split dataset function

--- utils/datasets.py
+++ utils/datasets.py
@@ -1,16 +1,15 @@
-import torch
-from torch import nn
-import torch.nn.functional as F
-
 import bisect
-import numpy as np
-
-import pandas as pd
 
 from braindecode.datasets.base import BaseConcatDataset
 from braindecode.datasets.sleep_physionet import SleepPhysionet
 from braindecode.datautil.preprocess import MNEPreproc, NumpyPreproc, preprocess, zscore
 from braindecode.datautil.windowers import create_windows_from_events
+import numpy as np
+import pandas as pd
+import torch
+from torch import nn
+import torch.nn.functional as F
+from torch.utils.data import ConcatDataset
 
 
 def get_sleep_physionet(subject_ids=range(83)):
@@ -18,7 +17,7 @@ def get_sleep_physionet(subject_ids=range(83)):
     subject_ids = [id for id in subject_ids if id not in bug_subjects]
 
     dataset = SleepPhysionet(
-        subject_ids=subject_ids, recording_ids=[0,1], crop_wake_mins=30)
+        subject_ids=subject_ids, recording_ids=[0, 1], crop_wake_mins=30)
     high_cut_hz = 30
 
     preprocessors = [
@@ -55,21 +54,59 @@ def get_sleep_physionet(subject_ids=range(83)):
     return windows_dataset, info
 
 
+def split_dataset(
+        windows_dataset, self_train_ratio=0.7, self_valid_ratio=0.1,
+        valid_ratio=0.1, test_ratio=0.1):
+    """
+    70% for self_train_set
+    10% for self_valid_set
+    10% for supervised valid_set
+    10% for supervised test_set
+    The supervised train_set is identical to self_train_set + self_valid_set
+    """
+    assert (self_train_ratio + self_valid_ratio + valid_ratio + test_ratio != 1), "wrong dataset ratios"
+
+    splitted = windows_dataset.split('subject')
+    subjects = list(splitted.keys())
+    np.random.shuffle(subjects)
+    self_train_index = round(len(subjects)*self_train_ratio)
+    self_valid_index = self_train_index + round(len(subjects)*self_valid_ratio)
+    valid_index = self_valid_index + round(len(subjects)*valid_ratio)
+
+    self_train_subjects = subjects[:self_train_index]
+    self_valid_subjects = subjects[self_train_index:self_valid_index]
+    train_subjects = subjects[:self_valid_index]
+    valid_subjects = subjects[self_valid_index:valid_index]
+    test_subjects = subjects[valid_index:]
+
+    self_train_set = TimeContrastiveDataset([splitted[s] for s in self_train_subjects],
+                                            delta_index_positive=5, delta_index_negative=20)
+    self_valid_set = TimeContrastiveDataset([splitted[s] for s in self_valid_subjects],
+                                            delta_index_positive=5, delta_index_negative=20)
+    train_set = ConcatDataset([splitted[s] for s in train_subjects])
+    valid_set = ConcatDataset([splitted[s] for s in valid_subjects])
+    test_set = ConcatDataset([splitted[s] for s in test_subjects])
+
+    return self_train_set, self_valid_set, train_set, valid_set, test_set
+
+
 class TimeContrastiveDataset(BaseConcatDataset):
     """
     ----------
     list_of_ds: list
         list of BaseDataset, BaseConcatDataset or WindowsDataset
     """
+
     def __init__(self, list_of_ds, delta_index_positive=10, delta_index_negative=100):
         super(TimeContrastiveDataset, self).__init__(list_of_ds)
         self.delta_index_positive = delta_index_positive
         self.delta_index_negative = delta_index_negative
-    
+
     def __getitem__(self, idx):
         if idx < 0:
             if -idx > len(self):
-                raise ValueError("absolute value of index should not exceed dataset length")
+                raise ValueError(
+                    "absolute value of index should not exceed dataset length")
             idx = len(self) + idx
         dataset_idx = bisect.bisect_right(self.cumulative_sizes, idx)
         if dataset_idx == 0:
@@ -87,14 +124,14 @@ class TimeContrastiveDataset(BaseConcatDataset):
             else:
                 y = 0
                 contrastive_sample_idx = np.random.choice(np.concatenate([np.arange(0, max(0, anchor_sample_idx - self.delta_index_negative)),
-                                                                         np.arange(min(dataset_size, anchor_sample_idx + self.delta_index_negative + 1), dataset_size)]))
-        
+                                                                          np.arange(min(dataset_size, anchor_sample_idx + self.delta_index_negative + 1), dataset_size)]))
+
         anchor_sample = self.datasets[dataset_idx][anchor_sample_idx]
         contastive_sample = self.datasets[dataset_idx][contrastive_sample_idx]
 
         X = list(zip(*[anchor_sample, contastive_sample]))[0]
         X = np.array(X)
-        
+
         return X, y
 
 
@@ -104,35 +141,38 @@ class MultipleWindowsDataset(BaseConcatDataset):
     list_of_ds: list
         list of BaseDataset, BaseConcatDataset or WindowsDataset
     """
+
     def __init__(self, list_of_ds, number_of_windows=10, training=True):
         super(MultipleWindowsDataset, self).__init__(list_of_ds)
         self.number_of_windows = number_of_windows
         self.training = training
-        
+
     def __len__(self):
         if self.training:
             return super().__len__() // self.number_of_windows
         else:
             return super().__len__()
-    
+
     def __getitem__(self, idx):
         if self.training:
-            idx = idx * self.number_of_windows + np.random.randint(0, self.number_of_windows)
+            idx = idx * self.number_of_windows + \
+                np.random.randint(0, self.number_of_windows)
 
         if idx < 0:
             if -idx > len(self):
-                raise ValueError("absolute value of index should not exceed dataset length")
+                raise ValueError(
+                    "absolute value of index should not exceed dataset length")
             idx = len(self) + idx
         dataset_idx = bisect.bisect_right(self.cumulative_sizes, idx)
         if dataset_idx == 0:
             center_idx = idx
         else:
             center_idx = idx - self.cumulative_sizes[dataset_idx - 1]
-            
+
         center_crop = center_idx % self.number_of_windows
-                                                     
+
         dataset_size = len(self.datasets[dataset_idx])
-        
+
         if center_idx < self.number_of_windows // 2:
             center_idx = self.number_of_windows // 2
         elif center_idx > dataset_size - self.number_of_windows // 2 - 1:
@@ -142,15 +182,15 @@ class MultipleWindowsDataset(BaseConcatDataset):
         end_idx = center_idx + self.number_of_windows // 2
         if end_idx == start_idx:
             end_idx += 1
-    
+
         windows = []
         labels = []
         #pos = []
         for i in range(start_idx, end_idx):
             windows.append(self.datasets[dataset_idx][i][0])
             labels.append(self.datasets[dataset_idx][i][1])
-            #pos.append(i)
-        
+            # pos.append(i)
+
         X = np.array(windows)
         y = np.array(labels)
         if self.training:
