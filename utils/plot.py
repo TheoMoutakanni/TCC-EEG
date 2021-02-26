3ca76b7acdcf6e5daab79171e0c37af528d74f1e
Clément Bonnet <clement.bonnet16@gmail.com>
2021-02-26 12:28:00 +0100

    added plot functions

--- utils/plot.py
+++ utils/plot.py
@@ -1,10 +1,24 @@
-import matplotlib as mpl
-import matplotlib.pyplot as plt
-from matplotlib.lines import Line2D
+import colorsys
 
+from braindecode.datasets.base import BaseConcatDataset
+import matplotlib as mpl
+from matplotlib.colors import LinearSegmentedColormap
+from matplotlib.lines import Line2D
+import matplotlib.pyplot as plt
+import numpy as np
+from sklearn.decomposition import PCA
+from sklearn.manifold import TSNE
 
 sleep_cmap = mpl.colors.LinearSegmentedColormap.from_list('Custom cmap', ['r', 'g', 'b', 'pink', 'orange'], len(['r', 'g', 'b', 'pink', 'orange']))
-
+mapping = {  # We merge stages 3 and 4 following AASM standards.
+    'Sleep stage W': 0,
+    'Sleep stage 1': 1,
+    'Sleep stage 2': 2,
+    'Sleep stage 3': 3,
+    'Sleep stage 4': 3,
+    'Sleep stage R': 4
+}
+reverse_mapping = {v: k[-1] for k,v in mapping.items()}
 
 def rand_cmap(nlabels, type='bright', first_color_black=True, last_color_black=False, verbose=True):
     """
@@ -16,11 +30,6 @@ def rand_cmap(nlabels, type='bright', first_color_black=True, last_color_black=F
     :param verbose: Prints the number of labels and shows the colormap. True or False
     :return: colormap for matplotlib
     """
-    from matplotlib.colors import LinearSegmentedColormap
-    import colorsys
-    import numpy as np
-
-
     if type not in ('bright', 'soft'):
         print ('Please choose "bright" or "soft" for type')
         return
@@ -75,3 +84,95 @@ def rand_cmap(nlabels, type='bright', first_color_black=True, last_color_black=F
                                    boundaries=bounds, format='%1i', orientation=u'horizontal')
 
     return random_colormap
+
+
+# Sleep stages
+
+def plot_sleep_stages_pca(pca_embds, windows_dataset, subjects_dic):
+    """
+    Plot PCA of sleep stages using the embeddings.
+    """
+    sleep_stages = [x[1] for x in BaseConcatDataset([windows_dataset.split('subject')[s] for s in subjects_dic["self_valid_subjects"]])]
+    plt.figure(figsize=(14,12))
+    scatter = plt.scatter(pca_embds[:,0], pca_embds[:,1], c=sleep_stages, cmap=sleep_cmap, alpha=0.3, s=6)
+    legend = plt.legend(*scatter.legend_elements(prop="colors"),
+                        loc="lower right", title="Sleep stages")
+    for i in range(len(legend.get_texts())):
+        legend.get_texts()[i].set_text(reverse_mapping[i])
+    plt.gca().add_artist(legend)
+    plt.title("PCA of self-supervised embeddings according to sleep stages")
+
+
+def plot_sleep_stages_tsne(tsne_embds, windows_dataset, subjects_dic):
+    """
+    Plot TSNE of sleep stages using the embeddings.
+    """
+    sleep_stages = [x[1] for x in BaseConcatDataset([windows_dataset.split('subject')[s] for s in subjects_dic["self_valid_subjects"]])]
+    plt.figure(figsize=(14,12))
+    scatter = plt.scatter(tsne_embds[:,0], tsne_embds[:,1], c=sleep_stages, cmap=sleep_cmap, alpha=0.3, s=6)
+    legend = plt.legend(*scatter.legend_elements(prop="colors"),
+                        loc="lower right", title="Sleep stages")
+    for i in range(len(legend.get_texts())):
+        legend.get_texts()[i].set_text(reverse_mapping[i])
+    plt.gca().add_artist(legend)
+    plt.title('TSNE of self-supervised embeddings according to sleep stages')
+
+
+# Age
+
+def plot_age_pca(pca_embds, windows_dataset, subjects_dic, info):
+    """
+    Plot PCA of patient ages using the embeddings.
+    """
+    ages = [i for s in subjects_dic["self_valid_subjects"] 
+        for i in [info[info['subject'] == int(s)]['age'].iloc[0]]*len(windows_dataset.split('subject')[s])]
+    plt.figure(figsize=(14,12))
+    plt.scatter(pca_embds[:,0], pca_embds[:,1], c=ages, alpha=0.3, s=6, cmap=plt.get_cmap('inferno'))
+    plt.colorbar()
+    plt.title('PCA of self-supervised embeddings according to age')
+
+
+def plot_age_tsne(tsne_embds, windows_dataset, subjects_dic, info):
+    """
+    Plot TSNE of patient ages using the embeddings.
+    """
+    ages = [i for s in subjects_dic["self_valid_subjects"] 
+        for i in [info[info['subject'] == int(s)]['age'].iloc[0]]*len(windows_dataset.split('subject')[s])]
+    plt.figure(figsize=(14,12))
+    plt.scatter(tsne_embds[:,0], tsne_embds[:,1], c=ages, alpha=0.3, s=6, cmap=plt.get_cmap('inferno'))
+    plt.colorbar()
+    plt.title('TSNE of self-supervised embeddings according to age')
+
+
+# Patient ID
+
+def plot_patient_id_pca(pca_embds, windows_dataset, subjects_dic):
+    """
+    Plot PCA of patient ids using the embeddings.
+    """
+    subject = [int(i) for s in subjects_dic["self_valid_subjects"]
+        for i in [s]*len(windows_dataset.split('subject')[s])]
+    new_cmap = rand_cmap(
+        max(subject)+1, type='bright', first_color_black=True,
+        last_color_black=False, verbose=False)
+    plt.figure(figsize=(14,12))
+    plt.scatter(pca_embds[:,0], pca_embds[:,1], c=subject, alpha=0.3, s=6, cmap=new_cmap)
+    plt.colorbar()
+    plt.title('PCA of self-supervised embeddings according to patient id')
+    plt.show()
+
+
+def plot_patient_id_tsne(tsne_embds, windows_dataset, subjects_dic):
+    """
+    Plot TSNE of patient ids using the embeddings.
+    """
+    subject = [int(i) for s in subjects_dic["self_valid_subjects"]
+        for i in [s]*len(windows_dataset.split('subject')[s])]
+    new_cmap = rand_cmap(
+        max(subject)+1, type='bright', first_color_black=True,
+        last_color_black=False, verbose=False)
+    plt.figure(figsize=(14,12))
+    plt.scatter(tsne_embds[:,0], tsne_embds[:,1], c=subject, alpha=0.3, s=6, cmap=new_cmap)
+    plt.colorbar()
+    plt.title('TSNE of self-supervised embeddings according to patient id')
+    plt.show()
